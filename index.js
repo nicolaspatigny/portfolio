1d72252bb469dc6a8dcf61a524f88f42de9a0c95
unknown <nickim.patigny@gmail.com>
2025-05-08 22:42:25 +0200

    Updates

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 var vite_config_default = defineConfig({
+  base: "/portfolio/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
